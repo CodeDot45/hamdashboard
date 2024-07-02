a3d0764bc41b4e7c96fa83604e5289cb4396a4ad
CodeDot45 <89028104+CodeDot45@users.noreply.github.com>
2024-07-01 21:30:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const topBarCenterText = `VA3HDL - FN04ga`;
+const topBarCenterText = `W5BIC`;
 // Menu items
 // Structure is as follows HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
 // The values are [color code, menu text, target link, scale factor, side],
@@ -58,7 +58,8 @@ const aIMG = [
   ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
   [
     "LOCAL RADAR",
-    "https://radar.weather.gov/ridge/standard/KNQA_loop.gif",
+    "https://radar.weather.gov/ridge/standard/SOUTHPLAINS_loop.gif",
+    "https://radar.weather.gov/ridge/standard/KGRK_loop.gif"
   ],
   [
     "NOAA D-RAP",
@@ -71,11 +72,11 @@ const aIMG = [
   ],
   [
     "SATELLITE CAN",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/can/EXTENT3/GOES16-CAN-EXTENT3-1125x560.gif",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/sp/EXTENT3/GOES16-SP-EXTENT3-600x600.gif",
   ],
   [
     "SATELLITE CGL",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/cgl/EXTENT3/GOES16-CGL-EXTENT3-600x600.gif",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/sp/EXTENT3/2400x2400.jpg",
   ],
   [
     "LIGHTNING",
@@ -83,16 +84,31 @@ const aIMG = [
   ],
   [
     "LIGHTNING LOCAL",
-    "https://www.blitzortung.org/en/Images/image_b_ny.png",
+    "https://www.blitzortung.org/en/Images/image_b_tx.png",
   ],
   ["GREY LINE", "https://www.timeanddate.com/scripts/sunmap.php?iso=now"],
   [
-    "SW BROADCAST",
-    "https://www.short-wave.info/php/transmitter-site-map.php?mobile=false&lat=52.67|-21.96|-15.53|-9.42|-17.76|-17.53|46.34|50.73|42.81|39.75|50.89|29.60|6.23|39.40|-15.53|43.51|46.34|-21.96|34.38|44.15|39.36|46.34|39.91|39.91|46.34|27.46|24.88|27.46|36.28|39.36|42.04|36.28|36.21|12.69|18.22|24.17|42.04|29.60|-15.73|-7.90|36.21|12.69|36.21|29.15|30.65|-21.96|33.50|-38.83|36.28|36.21|27.46&lon=9.75|27.60|28.00|160.05|168.36|146.05|-67.83|4.39|23.19|116.81|-113.85|55.79|-10.70|32.86|28.00|-79.63|-67.83|27.64|108.61|86.90|75.72|-67.83|-76.58|-76.58|-67.83|-80.93|102.50|-80.93|-86.10|75.72|12.32|-86.10|-86.89|-8.02|-63.02|54.25|12.32|55.79|46.45|-14.38|-86.89|-8.02|-86.89|47.77|-87.09|27.64|-86.47|176.42|-86.10|-86.89|-80.93&freq=3975|4930|4965|5020|5040|5055|5130|5780|5900|5985|6030|6040|6050|6050|6065|6070|6160|6195|7285|7295|7415|7490|9265|9265|9330|9395|9440|9455|9475|9600|9705|9930|9980|11640|11775|11810|11870|11880|11965|12095|12160|13630|13845|15540|15555|15580|15610|15720|15810|15825|17790&az=ND|20|ND|ND|ND|ND|245|ND|126|257|ND|313|ND|310|315|ND|245|350|317|270|308|245|242|242|245|355|283|285|50|308|206|180|90|111|320|90|210|211|295|27|85|111|90|310|5|350|85|35|40|46|160",
+    "Forecast & Activity", 
+    "https://www.wpc.ncep.noaa.gov/noaa/noaa.gif", 
+    "https://www.spc.noaa.gov/exper/mesoanalysis/activity_loop.gif", 
+    "https://www.spc.noaa.gov/products/watch/validww.png", 
+    "https://www.spc.noaa.gov/products/exper/day4-8/day48prob.gif", 
+    "https://www.wpc.ncep.noaa.gov/threats/final/hazards_d3_7_contours.png", 
+    "https://www.wpc.ncep.noaa.gov/qpf/fill_94qwbg.gif",
   ],
-  ["10M PROPAGATION", "https://www.tvcomm.co.uk/g7izu/Autosave/NA_ES_AutoSave.JPG"],
-  ["HF PROPAGATION",
-  "https://www.hamqsl.com/solar101vhf.php",
-  "https://www.hamqsl.com/solar100sc.php",
-  "https://www.hamqsl.com/solarpich.php"],
+  ["DAILY FORECAST", 
+  "https://www.weather.gov/images/fwd/graphicast/image_full1.jpg", 
+  "https://www.weather.gov/images/fwd/graphicast/image_full2.jpg", 
+  "https://www.weather.gov/images/fwd/graphicast/image_full3.jpg", 
+  "https://www.weather.gov/images/fwd/adas/temp_sfc_loop.gif", 
+  "https://www.weather.gov/images/fwd/adas/heatindex_sfc_loop.gif", 
+  "https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/uvi_map.gif"
+  ],
+  ["Tropics", 
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/Sandwich/GOES16-TAW-Sandwich-900x540.gif", 
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/se/Sandwich/1200x1200.jpg", 
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/car/Sandwich/500x500.jpg", 
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/gm/Sandwich/500x500.jpg", 
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/Sandwich/900x540.jpg"
+  ],
 ];
